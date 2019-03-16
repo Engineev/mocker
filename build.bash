f5bf234064e4444bd4568f1b619378ff49d9b12a
Engineev <ryw_pub@126.com>
2019-03-16 22:16:31 +0800

    Add OJ support (semantic)

--- build.bash
+++ build.bash
@@ -3,9 +3,14 @@
 set -e
 cd "$(dirname "$0")"
 
-if [[ ! -d "./build/" ]]; then
-  mkdir build;
-fi;
-cd build
-cmake ..
-make
+# if [[ ! -d "./build/" ]]; then
+#   mkdir build;
+# fi;
+# cd build
+# cmake ..
+# make
+
+g++ -O2 -std=c++14 -I./support -I./ir/include -I./ir/include/ir -I./compiler \
+  ./ir/src/*.cpp ./compiler/ir_builder/*.cpp ./compiler/optim/*.cpp \
+  ./compiler/parse/*.cpp ./compiler/semantic/*.cpp ./compiler/main.cpp \
+  -o mocker-c
--- semantic.bash
+++ semantic.bash
@@ -4,5 +4,8 @@ set -e
 cd "$(dirname "$0")"
 
 cat > program.in
-./build/compiler/mocker-c ./program.in
+
+# ./build/compiler/mocker-c ./program.in  # cmake version
+./mocker-c ./program.in
+
 # rm ./program.in
