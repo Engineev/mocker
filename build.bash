88cc1a650d984acd275abca37abe2c00293e2fda
Engineev <ryw_pub@126.com>
2019-04-06 00:06:35 +0800

    disable forward reference for global variables

--- build.bash
+++ build.bash
@@ -18,7 +18,8 @@ g++ -c -O2 -std=c++14 -I./support \
 ar rvs libnasm.a *.o
 rm *.o
 # compiler
-g++ -O2 -std=c++14 -I./support -I./ir/include -I./nasm/include -I./compiler \
+g++ -DDISABLE_FORWARD_REFERENCE_FOR_GLOBAL_VAR \
+  -O2 -std=c++14 -I./support -I./ir/include -I./nasm/include -I./compiler \
   ./compiler/ir_builder/*.cpp ./compiler/optim/*.cpp ./compiler/codegen/*.cpp \
   ./compiler/parse/*.cpp ./compiler/semantic/*.cpp ./compiler/main.cpp \
   -o mocker-c \
